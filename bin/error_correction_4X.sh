bdab58afd79c7cf4482dc573ae01c7f30dac2a36
Max Shen <maxwshen@gmail.com>
2015-01-12 14:13:45 -0800

    new ec

---
+++ bin/error_correction_4X.sh
@@ -0,0 +1,5 @@
+#! /bin/csh -f
+/home/jeyuan/blasr/alignment/bin/blasr $argv[2] $argv[1] -maxMatch 15 -minMatch 10 -bestn 1 -m 5 -out v0_$argv[1].out;
+/home/lin/program/process_alignment_0108 v0_$argv[1].out 4X_$argv[1] 4;
+rm v0_$argv[1].out;
+
