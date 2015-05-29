07e61dc9de33700075ecaa8d7d23b95959a35bb6
Max Shen <maxwshen@gmail.com>
2015-05-29 21:03:01 +0200

    bug fix

--- bin/fig_etc5.py
+++ bin/fig_etc5.py
@@ -2,10 +2,10 @@
 import itec4
 import read_fasta as rf
 
-reads_file = '/home/mshen/research/data/reads.20k.rc.fasta'
-true_nhoods = '/home/mshen/research/data/genome_nhood.txt'
-ktmer_headers_file = '/home/mshen/research/data/20k_v2/temp_ktmer_headersrx_27_6_rc_v2.out'
-creads_file = '/home/mshen/research/data/20k_v2/temp_creads.outrx_27_6_rc_v2.out'
+reads_file = '/home/yu/max/research/data/reads.20k.rc.fasta'
+true_nhoods = '/home/yu/max/research/data/genome_nhood.txt'
+ktmer_headers_file = '/home/yu/max/research/data/20k_v2/temp_ktmer_headersrx_27_6_rc_v2.out'
+creads_file = '/home/yu/max/research/data/20k_v2/temp_creads.outrx_27_6_rc_v2.out'
 
 hr, rr = rf.read_fasta(reads_file)
 headers = itec4.build_headers_dict(ktmer_headers_file)
@@ -15,7 +15,9 @@ for i in range(len(hr)):
 ktmers = headers.keys()
 
 header = '>R_m140909_094518_42139_c100697390480000001823143803261574_s1_p0/150325/0_32327'
-nhood = itec4.get_simple_1_deg_nhood(header, creads, headers, hr):
+nhood, windows = itec4.get_simple_1_deg_nhood(header, creads, headers, hr)
+nhood = list(nhood)
 nhood_indices = [hr.index(s) for s in nhood]
 
 itec4.nhood_stats(hr.index(header), nhood_indices)
+print len(nhood)
