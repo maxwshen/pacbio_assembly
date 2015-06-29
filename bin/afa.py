554536e09227305645fe97946d996d11480b207f
Max Shen <maxwshen@gmail.com>
2015-06-29 10:00:43 -0700

    more work on afa

--- bin/afa.py
+++ bin/afa.py
@@ -1,6 +1,6 @@
 # Alignment-Free Assembly by finding the most read-supported path
 
-import sys, string, datetime, random, copy, os, commands, fnmatch
+import sys, string, datetime, random, copy, os, commands, fnmatch, re
 import numpy as np
 from collections import defaultdict
 
@@ -37,17 +37,20 @@ def afa(reads_fn, ktmer_headers_fn, creads_fn):
   random.shuffle(ktmers)
   print 'Found', len(ktmers), 'kt-mers.'
 
+  num_shared_cutoff = 6
   ktmer = 'TTTTCATCTGGAAGCGATTCTCGGGGA'   # pos = 4478401
   print gr.find(ktmer)
 
-  traversed = TraversedReads(ktmer, creads, headers)
+  seenReads = TraversedReads(ktmer, creads, headers)
+  traversed = set(ktmer)
   while True:
-    ktmer = move(ktmer, traversed, creads, headers, gr)
-    print gr.find(ktmer)
+    ktmer = move(ktmer, seenReads, traversed, creads, headers, gr, num_shared_cutoff)
+    # print gr.find(ktmer)
+    print 'pos: ', ', '.join([str(s.start()) for s in re.finditer(ktmer, gr)])
 
   return
 
-def move(ktmer, traversed, creads, headers, gr):
+def move(ktmer, seenReads, traversed, creads, headers, gr, num_shared_cutoff):
   def find_candidates(ktmer, creads, headers):
     reads = headers[ktmer]
     next_ktmers = []
@@ -62,28 +65,45 @@ def move(ktmer, traversed, creads, headers, gr):
   def shared_reads(base_reads, new_reads):
     return len(base_reads.intersection(new_reads))
 
-  def get_best_candidate(cands, traversed, creads, headers):
+  def score(kmer_pos, first_pos):
+    if kmer_pos > first_pos:
+      return (kmer_pos - first_pos) ** 2
+    else:
+      return 0
+
+  def get_best_candidate(cands, seenReads, creads, headers, num_shared_cutoff):
+    if len(cands) == 0:
+      print 'No candidates found'
+      return ''
+
     scores = []
     for ckmer in cands:
+      scoresum = 0
+      num_shared = 0
       for r in headers[ckmer]:
-        score = 0
-        if r in traversed.tr:
-          score += (get_pos_in_read(ckmer, creads[r]) - traversed.get(r)) ** 2
-      scores.append(score)
+        if r in seenReads.tr:
+          num_shared += 1
+          scoresum += score(get_pos_in_read(ckmer, creads[r], seenReads.get(r)))
+      if num_shared <= num_shared_cutoff:
+        scoresum = 0
+      scores.append(scoresum)
     sorted_cands = [x for (y,x) in sorted(zip(scores, cands))]
     print '  ', sorted(scores), '\n  ', [gr.find(s) for s in sorted_cands]
     return sorted_cands[-1]
 
-  cands, dists = find_candidates(ktmer, creads, headers)
-  # curr_reads = set(headers[ktmer])
-  # cands.sort(key = lambda x : shared_reads(curr_reads, headers[x]), reverse = True)
-  if len(cands) > 0:
-    new_ktmer = get_best_candidate(cands, traversed, creads, headers)
-  else:
-    new_ktmer = ''
-    print 'No candidates found'
 
-  traversed.update(new_ktmer, creads, headers)
+  cands, dists = find_candidates(ktmer, creads, headers)
+  cands = [s for s in cands if s not in traversed]
+  new_ktmer = get_best_candidate(cands, seenReads, creads, headers, num_shared_cutoff)
+  while new_ktmer == '':
+    num_shared_cutoff -= 1
+    if num_shared_cutoff < 0:
+      print 'num_shared_cutoff is less than 0 and still no candidates were found'
+      sys.exit(0)
+    new_ktmer = get_best_candidate(cands, seenReads, creads, headers, num_shared_cutoff)
+
+  seenReads.update(new_ktmer, creads, headers)
+  traversed.add(new_ktmer)
   return new_ktmer
 
 def get_pos_in_read(kmer, cread):
